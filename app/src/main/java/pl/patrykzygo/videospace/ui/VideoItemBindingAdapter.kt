c27b5daeb219a86bee1e4f7c22b9562bade66891
DESKTOP-Mallorax <patryk733773@gmail.com>
2022-02-01 20:42:08 +0100

    Added poster loading

--- app/src/main/java/pl/patrykzygo/videospace/ui/VideoItemBindingAdapter.kt
+++ app/src/main/java/pl/patrykzygo/videospace/ui/VideoItemBindingAdapter.kt
@@ -1,9 +1,19 @@
 package pl.patrykzygo.videospace.ui
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import pl.patrykzygo.videospace.R
+import pl.patrykzygo.videospace.others.Constants
 
-@BindingAdapter("popularity")
-fun bindPopularity(textView: TextView, popularity: Double) {
-    textView.text = "Popularity: $popularity"
+
+@BindingAdapter("moviePoster")
+fun bindMoviePoster(imageView: ImageView, posterPath: String){
+    Glide.with(imageView)
+        .load(Constants.POSTERS_BASE_URL + posterPath)
+        .error(R.drawable.ic_baseline_no_photography_24)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(imageView)
 }
