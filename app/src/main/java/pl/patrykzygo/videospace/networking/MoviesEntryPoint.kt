d719f0d53584ba14b82affbbce17facc4707d754
DESKTOP-Mallorax <patryk733773@gmail.com>
2022-02-04 15:58:52 +0100

    Added rest of the requests to entry point that will be needed later

--- app/src/main/java/pl/patrykzygo/videospace/networking/MoviesEntryPoint.kt
+++ app/src/main/java/pl/patrykzygo/videospace/networking/MoviesEntryPoint.kt
@@ -4,6 +4,7 @@ import pl.patrykzygo.videospace.data.network.PopularMoviesResponse
 import pl.patrykzygo.videospace.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesEntryPoint {
@@ -16,6 +17,46 @@ interface MoviesEntryPoint {
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1,
         @Query("region") region: String? = null
+    ): Response<PopularMoviesResponse>
+
+    @GET("top_rated")
+    suspend fun requestTopRatedMovies(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Query("region") region: String? = null
+    ): Response<PopularMoviesResponse>
+
+    @GET("now_playing")
+    suspend fun requestNowPlayingMovies(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Query("region") region: String? = null
+    ): Response<PopularMoviesResponse>
+
+    @GET("upcoming")
+    suspend fun requestUpcomingMovies(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Query("region") region: String? = null
+    ): Response<PopularMoviesResponse>
+
+    @GET("{id}/recommendations")
+    suspend fun requestRecommendationsForMovie(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Path("id") id: Int
+    ): Response<PopularMoviesResponse>
+
+    @GET("{id}/similar")
+    suspend fun requestSimilarMovies(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Path("id") id: Int
     ): Response<PopularMoviesResponse>
 
 }
