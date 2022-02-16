fd527da6e44500d99cba702ad7ed53a031e68ae4
DESKTOP-KDGIGUC\Patryk <patryk733773@gmail.com>
2022-02-16 14:18:10 +0100

    Imports and formatting

--- app/src/main/java/pl/patrykzygo/videospace/ui/movies_list/MoviesListFragment.kt
+++ app/src/main/java/pl/patrykzygo/videospace/ui/movies_list/MoviesListFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/pl/patrykzygo/videospace/ui/movies_list/MoviesListVMFactory.kt
+++ app/src/main/java/pl/patrykzygo/videospace/ui/movies_list/MoviesListVMFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import pl.patrykzygo.videospace.repository.MoviesPagingSource
 
-class MoviesListVMFactory(val repo: MoviesPagingSource): ViewModelProvider.Factory {
+class MoviesListVMFactory(val repo: MoviesPagingSource) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(MoviesPagingSource::class.java)
