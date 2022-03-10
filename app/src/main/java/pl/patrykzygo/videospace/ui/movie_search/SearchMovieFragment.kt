d1e848b5b3b75c378b85e2069f5297e9710b4781
DESKTOP-KDGIGUC\Patryk <patryk733773@gmail.com>
2022-03-10 19:41:59 +0100

    Search movie fragment layout

---
+++ app/src/main/java/pl/patrykzygo/videospace/ui/movie_search/SearchMovieFragment.kt
@@ -0,0 +1,8 @@
+package pl.patrykzygo.videospace.ui.movie_search
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchMovieFragment: Fragment() {
+}
---
+++ app/src/main/java/pl/patrykzygo/videospace/ui/movie_search/SearchMovieViewModel.kt
@@ -0,0 +1,7 @@
+package pl.patrykzygo.videospace.ui.movie_search
+
+import androidx.lifecycle.ViewModel
+import pl.patrykzygo.videospace.repository.local_store.LocalStoreRepository
+
+class SearchMovieViewModel constructor(private val repo: LocalStoreRepository): ViewModel() {
+}
