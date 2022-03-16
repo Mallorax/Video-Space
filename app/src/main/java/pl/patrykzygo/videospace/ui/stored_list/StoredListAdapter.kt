136b880197ecc0ba6529648105f0c1c9eea0e92f
DESKTOP-KDGIGUC\Patryk <patryk733773@gmail.com>
2022-03-16 10:25:43 +0100

    Finishing stored list fragment

--- app/src/main/java/pl/patrykzygo/videospace/ui/stored_list/StoredListAdapter.kt
+++ app/src/main/java/pl/patrykzygo/videospace/ui/stored_list/StoredListAdapter.kt
@@ -1,4 +1,47 @@
 package pl.patrykzygo.videospace.ui.stored_list
 
-class StoredListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import pl.patrykzygo.videospace.data.app.Movie
+import pl.patrykzygo.videospace.databinding.MovieListItemBindingImpl
+import pl.patrykzygo.videospace.ui.view_holders.MovieListItemViewHolder
+
+class StoredListAdapter(private val onMovieClickListener: OnMovieClickListener) :
+    ListAdapter<Movie, MovieListItemViewHolder>(DiffCallback) {
+
+    override fun onBindViewHolder(holderListItem: MovieListItemViewHolder, position: Int) {
+        val movie = getItem(position)
+        val binding = holderListItem.binding
+        binding.root.setOnClickListener {
+            onMovieClickListener.onMovieClick(movie, it)
+        }
+        if (movie != null) {
+            holderListItem.bind(movie)
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MovieListItemBindingImpl.inflate(inflater, parent, false)
+        return MovieListItemViewHolder(binding)
+    }
+
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.originalTitle == newItem.originalTitle
+        }
+    }
+
+    class OnMovieClickListener(val clickListener: (movie: Movie?, view: View) -> Unit) {
+        fun onMovieClick(movie: Movie?, view: View) = clickListener(movie, view)
+    }
 }
--- app/src/main/java/pl/patrykzygo/videospace/ui/stored_list/StoredListFragment.kt
+++ app/src/main/java/pl/patrykzygo/videospace/ui/stored_list/StoredListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import pl.patrykzygo.videospace.databinding.FragmentStoredListBinding
 import pl.patrykzygo.videospace.ui.factories.MainViewModelFactory
@@ -17,6 +19,7 @@ class StoredListFragment(
 
     private var _binding: FragmentStoredListBinding? = null
     val binding get() = _binding!!
+    private val adapter = createRecyclerViewAdapter()
 
     lateinit var viewModel: StoredListViewModel
 
@@ -33,9 +36,25 @@ class StoredListFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding.storedListRecycler
+        recyclerView.adapter = adapter
 
+        subscribeObservers()
+    }
+    private fun createRecyclerViewAdapter(): StoredListAdapter{
+        return StoredListAdapter(StoredListAdapter.OnMovieClickListener{ movie, view ->
+            val bundle = Bundle()
+            bundle.putParcelable("movie", movie)
+            parentFragmentManager.setFragmentResult("storedListResult", bundle)
+        })
     }
 
     private fun subscribeObservers(){
+        viewModel.movies.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer {
+            Snackbar.make(requireView(), it, Snackbar.LENGTH_LONG).show()
+        })
     }
 }
